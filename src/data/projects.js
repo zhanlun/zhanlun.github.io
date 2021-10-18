4202e290948834e248e1d281b418d02e7466eda6
Zhan Lun Ng <zlun0817@gmail.com>
2021-10-18 15:12:33 +0800

    Update projects data

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,25 +1,6 @@
 module.exports = {
   projectsData: [
     {
-      img: './img/piano.png',
-      title: 'Piano App',
-      subtitle: 'A piano app with multiple features',
-      linkListItems: [
-        {
-          text: 'Demo',
-          url: 'https://zhanlun.github.io/piano'
-        },
-        {
-          text: 'Source Code',
-          url: 'https://github.com/zhanlun/piano'
-        },
-      ],
-      bottomSubtitle: 'Technologies Used',
-      bottomListItems: [
-        'React', 'Web Audio API', 'CSS'
-      ]
-    },
-    {
       img: './img/task.png',
       title: 'Task Management App',
       subtitle: 'An interactive kanban style task management app, similar to Trello',
@@ -43,6 +24,25 @@ module.exports = {
       ]
     },
     {
+      img: './img/piano.png',
+      title: 'Piano App',
+      subtitle: 'A piano app with multiple features',
+      linkListItems: [
+        {
+          text: 'Demo',
+          url: 'https://zhanlun.github.io/piano'
+        },
+        {
+          text: 'Source Code',
+          url: 'https://github.com/zhanlun/piano'
+        },
+      ],
+      bottomSubtitle: 'Technologies Used',
+      bottomListItems: [
+        'React', 'Web Audio API', 'CSS'
+      ]
+    },
+    {
       img: './img/portfolio.png',
       title: 'My Personal Website',
       subtitle: 'This website. Designed & developed by me',
@@ -55,6 +55,29 @@ module.exports = {
       bottomSubtitle: 'Technologies Used',
       bottomListItems: [
         'React', 'Tailwind CSS',
+      ]
+    },
+    {
+      img: './img/library.png',
+      title: 'Library Management System',
+      subtitle: 'A demo project of library management system',
+      linkListItems: [
+        {
+          text: 'Demo',
+          url: 'https://zlun-library-management.netlify.app',
+        },
+        {
+          text: 'Source Code (Front-end)',
+          url: 'https://github.com/zhanlun/react-admin-library-management'
+        },
+        {
+          text: 'Source Code (Back-end)',
+          url: 'https://github.com/zhanlun/spring-demo-library-management'
+        },
+      ],
+      bottomSubtitle: 'Technologies Used',
+      bottomListItems: [
+        'Java', 'Spring Boot', 'React Admin',
       ]
     },
   ]
